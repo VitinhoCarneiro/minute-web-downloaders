57248f01fda0e5ddf95ac00ecffb1751cc2e1375
bitinhokaroneero <victorcarneiro2009@gmail.com>
2018-11-21 17:56:54 -0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# web-downloaders
+This repository contains my personal collection of Shell scripts for batch downloading of files from filehosting websites.
+
+They're really just little one-liners I've created and maintained for myself and a few internet friends, but I've decided to share them so anybody can study and use them.
+
+## About the files
+The scripts for each website lie in the main folder. They take a list of URLs from the standard input, one per line, and downloads each of them one by one. (I could make them download stuff in parallel, but some websites block that, plus I'm lazy)
+
+Since one-liners can be counter-intuitive, I maintain pretty-printed copies of them with insightful comments for better comprehension of the code. These can be found in the **formatted-code** folder.
+
+## Licensing
+Everything inside this repository is licensed under The Unlicense. More information can be found at unlicense.org.
---
+++ formatted-code/zippyshare-formatted.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# ZippyShare download script v4 - prettified version
+# For studying and better code comprehension. Even though it works just as well.
+while read url;
+do 
+	wget -O - $url | 
+		# Find the section containing the download button Javascript function
+		grep "dlbutton')" | 
+		# Parse the necessary values for generating the URL
+		sed 's/.*\"\(\/d\/[a-zA-Z0-9]*\/\)\"+.\([0-9]*\)%\([0-9]*\) + a() + b() + c() + d + 5\/5.+\"\(.*\)\";/\1 \2 \3 \4/g' | 
+		# Breakdown of the regex:
+		#### .*\" - matches everything up to a quote symbol
+		#### \(\/d\/[a-zA-Z0-9]*\/\) - captures the beginning of the resource URL which is
+		# written in verbatim and looks something like this: /d/qr2tiqa4/
+		#### \"+. - matches the closing quote, a plus symbol and the opening parenthesis for 
+		# the hash expression
+		#### ([0-9]*\)%\([0-9]*\) - captures the numerical values in the hash expression
+		# separately.
+		####  + a() + b() + c() + d + 5\/5.+\" - matches the rest of the hash expression
+		# which is just a bunch of weird function calls to functions which always return
+		# the same values. (what were they thinking?)
+		#### \(.*\)\"; - captures the file name to put in the end of the URL
+
+		# Calculate the hash and assemble the URL resource indicator
+		awk '{hash = ($2 % $3) + 1 + 2 + 3 + 4 + 5/5; print $1hash$4}' | 
+		# The constants above are the return values of a(), b() and c(), the value
+		# of d, and the constant 5/5, which are always the same for every page.
+
+		# Append the URL resource indicator to the URL's domain name
+		xargs -n1 printf "$(echo $url | sed 's/http\:\/\/\(www[0-9]*[.]zippyshare[.]com\).*/\1/gm')%s" | 
+		
+		xargs -n1 wget;
+done
+
